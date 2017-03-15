e1c594a2350fa113c61f06b559de2fadecc55cf0
Nick Nash <nicholas.nash@gmail.com>
2017-03-15 21:40:52 +0000

    Added cooperative scheduler

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ConsoleApplication
 {
@@ -6,11 +7,13 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            var test = new PetersenTest();
-            TestRunner.Run(test);
+            //var test = new PetersenTest();
+            //TestRunner.Run(test);
+            TestEnvironment.TE.SetupTest();
+            
         }
     }
-
+/*
     public class TestRunner 
     {
         private static TestEnvironment TE = TestEnvironment.TE;
@@ -28,32 +31,104 @@ namespace ConsoleApplication
     {
 
     }
-
+*/
     public class TestEnvironment
     {
-        public static TestEnvironment TE;
-
-        public ITest RunningTest;
+        public static TestEnvironment TE = new TestEnvironment();
     
-        private int _runningThread;
+        private int _runningThreadIdx;
         private int _numThreads;
+        private Thread[] _threads;
 
-        public TrackingData NewMemOrdered()
+        private bool[] _isRunning;
+        private Object[] _threadLocks;
+
+        private Random _random = new Random();
+
+        private void GenericThreadFunc(int idx)
         {
-            return null;
+            //int iter = 0;
+            while(true)
+            {
+                //Console.WriteLine($"Thread-{idx}, run number: {++iter}");
+                Console.WriteLine(idx);
+                //Thread.Sleep(500);
+                Scheduler();
+            }
+        }
+
+        private void MakeThreadFunction(Action threadFunction, int threadIdx)
+        {
+            var l = _threadLocks[threadIdx]; 
+            lock(l)
+            {
+                Monitor.Wait(l);
+            }
+            threadFunction();
+        }
+
+        public void SetupTest()
+        {
+            _numThreads = 5;
+            _threads = new Thread[_numThreads];
+            _isRunning = new bool[_numThreads];
+            _threadLocks = new Object[_numThreads];
+            for(int i = 0; i < _numThreads; ++i)
+            {
+                _threadLocks[i] = new Object();
+                int j = i;
+                Action threadFunc = () => GenericThreadFunc(j);
+                Action wrapped = () => MakeThreadFunction(threadFunc, j); 
+                _threads[i] = new Thread(new ThreadStart(wrapped));
+                _threads[i].Start();
+            }
+            Thread.Sleep(100); // TODO: Sync. properly with all threads going to sleep. Prevent a hang!
+            WakeThread(0);
+            Console.WriteLine("startup thread exiting!");
+
+        }
+
+        private void WakeThread(int idx)
+        {
+            _runningThreadIdx = idx;
+            _isRunning[idx] = true;
+            var l = _threadLocks[idx];
+            lock(l)
+            {
+                Monitor.Pulse(l);
+            }
         }
 
         public void Scheduler()
         {
-            _runningThread = (_runningThread + 1) % _numThreads;
-            RunningTest.Run(_runningThread);
+            int prevThreadIdx = _runningThreadIdx;
+            int nextThreadIdx = GetNextThreadIdx();
+            //Console.Write(nextThreadIdx + " -> ");
+            _isRunning[prevThreadIdx] = false;
+            WakeThread(nextThreadIdx);             
+            var runningLock = _threadLocks[prevThreadIdx];
+            lock(runningLock)
+            {
+                while(!_isRunning[prevThreadIdx]) // I may get here, and be woken before I got a chance to sleep. That's OK. 
+                {
+                    Monitor.Wait(runningLock);
+                }
+
+            }        
+        }
+
+        private int GetNextThreadIdx()
+        {
+            return _random.Next(_numThreads);
+            //return (_runningThreadIdx + 1) % _numThreads;
         }
     }
-
+/*
     interface ITest 
     {
-        int NumThreads { get; }
+        IReadOnlyList<Action> Threads { get; }
         void Setup();
+
         void RunThread(int index);
 
     }
@@ -63,8 +138,24 @@ namespace ConsoleApplication
         private MemOrdered<int> flag0;
         private MemOrdered<int> flag1;
         private MemOrdered<int> turn;
-        
-        public int NumThreads => 2;
+
+
+        private List<Action> _threads = new List<Action>{Thread1, Thread2};
+        public IReadOnlyList<Action> Threads => _threads;
+
+        IEnumerable<int> Thread1()
+        {
+            flag0.Store(25);
+            yield return 0;
+            flag1.Store(35);
+
+        }
+
+        void Thread2()
+        {
+
+        }
+
 
         void Setup()
         {
@@ -99,4 +190,5 @@ namespace ConsoleApplication
             TE.scheduler();
         }
     }
+    */
 }
