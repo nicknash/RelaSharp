ba18a8e29c3ea20707476f3e155efbed62b1b72c
Nick Nash <nicholas.nash@gmail.com>
2017-04-06 22:47:57 +0100

    Change RVolatile to static-style interface

--- RVolatile.cs
+++ RVolatile.cs
@@ -3,22 +3,16 @@ using System.Runtime.CompilerServices;
 
 namespace RelaSharp
 {
-    class RVolatile<T> where T : IEquatable<T>
+    static class RVolatile<T> where T : IEquatable<T>
     {
-        private MemoryOrdered<T> _data;
-        public RVolatile()
+        public static T Read(MemoryOrdered<T> data, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
         {
-            _data = new MemoryOrdered<T>();
+            return data.Load(MemoryOrder.Acquire, memberName, sourceFilePath, sourceLineNumber);
         }
 
-        public T Read([CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+        public static void Write(MemoryOrdered<T> data, T newValue, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
         {
-            return _data.Load(MemoryOrder.Acquire, memberName, sourceFilePath, sourceLineNumber);
-        }
-
-        public void Write(T newData, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
-        {
-            _data.Store(newData, MemoryOrder.Release, memberName, sourceFilePath, sourceLineNumber);
+            data.Store(newValue, MemoryOrder.Release, memberName, sourceFilePath, sourceLineNumber);
         }
     }
 }
