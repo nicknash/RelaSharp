1f1ab90f96cd463fe6d815f6ce9270ca617c7dba
Nick Nash <nicholas.nash@gmail.com>
2017-04-14 11:02:12 +0100

    Add more detailed examples for Treiber stack

--- Examples/TreiberStack.cs
+++ Examples/TreiberStack.cs
@@ -8,12 +8,21 @@ namespace RelaSharp.Examples
         class Config
         {
             public readonly string Description;
-
+            public readonly int NumPushingThreads;
+            public readonly int NumPushedPerThread;
+            public readonly int NumPoppingThreads;
+            public readonly bool PushAllBeforePop;
+            public readonly Action PostCondition;
             public readonly bool ExpectedToFail;
 
-            public Config(string description, bool expectedToFail)
+            public Config(string description, int numPushing, int numPushedPerThread, int numPopping, bool pushAllBeforePop, Action postCondition, bool expectedToFail)
             {
                 Description = description;
+                NumPushingThreads = numPushing;
+                NumPushedPerThread = numPushedPerThread;
+                NumPoppingThreads = numPopping;
+                PushAllBeforePop = pushAllBeforePop;
+                PostCondition = postCondition;
                 ExpectedToFail = expectedToFail;
             }
         }
@@ -29,15 +38,15 @@ namespace RelaSharp.Examples
 
             public override string ToString()
             {
-                return $"{Value}"; // actually get mem address would be clearer.
+                return $"{Value}"; 
             }
         }
 
-        class LockFreeStack
+        class NaiveLockFreeStack
         {
             private MemoryOrdered<Node> _head;
 
-            public LockFreeStack()
+            public NaiveLockFreeStack()
             {
                 _head = new MemoryOrdered<Node>();
             }
@@ -53,19 +62,17 @@ namespace RelaSharp.Examples
                 } while(!_head.CompareExchange(newHead, currentHead, MemoryOrder.AcquireRelease));
             }
 
-            public int Pop()
+            public int? Pop()
             {
                 Node currentHead;
-                int result;
+                int? result;
                 do
                 {
                     currentHead = _head.Load(MemoryOrder.Relaxed);
-                    result = currentHead.Value;
-                } while(!_head.CompareExchange(currentHead.Next, currentHead, MemoryOrder.AcquireRelease));
+                    result = currentHead?.Value;
+                } while(!_head.CompareExchange(currentHead?.Next, currentHead, MemoryOrder.AcquireRelease));
                 return result;
             }
-
-            public bool IsEmpty => _head.Load(MemoryOrder.Acquire) == null;
         }
 
 
@@ -77,16 +84,16 @@ namespace RelaSharp.Examples
         private static TestEnvironment TE = TestEnvironment.TE;
         private IEnumerator<Config> _configs;
         private Config ActiveConfig => _configs.Current;
-        private LockFreeStack _stack;
-
-        int[] _pushed = new int[]{1,2,3,4,5};
-        List<int> _popped;
+        private NaiveLockFreeStack _stack;
+        private MemoryOrdered<bool> _pushingThreadFinished;
+        HashSet<int> _popped;
+        List<int> _poppedInOrder;
 
         public TreiberStack()
         {
-            ThreadEntries = new List<Action> {Thread1,Thread2};
-            // TODO: Make configs with several threads pushing/popping.
-            var configList = new List<Config>{new Config("One thread pushing, one thread popping", false), 
+            var configList = new List<Config>{new Config("3 threads pushing 5 elements each, interleaved with 2 threads popping, threads interleaved.", 3, 5, 2, false, VerifyAllPushedWerePopped, false), 
+                                              new Config("1 thread pushing 20 elements each, interleaved with 10 threads popping, threads interleaved.", 1, 20, 10, false, VerifyAllPushedWerePopped, false),
+                                              new Config("1 thread pushing 30 elements, then 5 threads popping, all pushes before any pops.", 1, 30, 5, true, VerifyAllPoppedInReverseOrder, false)
                                              };
             _configs = configList.GetEnumerator();
         }
@@ -96,47 +103,97 @@ namespace RelaSharp.Examples
             PrepareForNewConfig();
         }
 
-        public void Thread1()
+        private void VerifyAllPushedWerePopped()
         {
-           for(int i = 0; i < _pushed.Length; ++i)
-           {
-               _stack.Push(i);
-           }        
+            for(int i = 0; i < ActiveConfig.NumPushingThreads * ActiveConfig.NumPushedPerThread; ++i)
+            {
+                TE.Assert(_popped.Remove(i), $"Couldn't find {i} in data popped from stack");
+            }
+            TE.Assert(_popped.Count == 0, "More data popped than pushed!");
         }
 
-        public void Thread2()
+        private void VerifyAllPoppedInReverseOrder()
         {
-            int numPopped = 0;
-            while(numPopped < _pushed.Length)
+            int expectedNumPushed = ActiveConfig.NumPushingThreads * ActiveConfig.NumPushedPerThread; 
+            TE.Assert(_poppedInOrder.Count == expectedNumPushed, $"Too much data popped: expected {expectedNumPushed} but found {_poppedInOrder.Count}");
+            for(int i = 0; i < expectedNumPushed; ++i)
             {
-                if(!_stack.IsEmpty)
-                {
-                    _popped.Add(_stack.Pop());
-                    ++numPopped;
-                }
+                int expected = expectedNumPushed - i - 1;
+                TE.Assert(_poppedInOrder[i] == expected, $"Expected to find {expected} in popped list but found {_poppedInOrder[i]}.");
             }
         }
 
         public void OnFinished()
         {
-            for(int i = 0; i < _popped.Count; ++i)
+            ActiveConfig.PostCondition();
+        }
+
+        private Action MakePushingThread(int threadIndex, int numPushedPerThread)
+        {
+            return () => PushingThread(threadIndex, numPushedPerThread);
+        }
+
+        private Action MakePoppingThread()
+        {
+            return PoppingThread; 
+        }
+
+        private void PushingThread(int threadIndex, int numPushedPerThread)
+        {
+            for (int i = 0; i < numPushedPerThread; ++i)
             {
-                Console.Write(_popped[i] + " ");
+                _stack.Push(numPushedPerThread * threadIndex + i);
             }
-            Console.WriteLine();
-            TE.FailTest("Force failure for testing");
+            _pushingThreadFinished.Store(true, MemoryOrder.Release);
+        }
+
+        private void PoppingThread()
+        {
+            if(ActiveConfig.PushAllBeforePop)
+            {
+                while(!_pushingThreadFinished.Load(MemoryOrder.Acquire)) ;
+            }
+            while (_popped.Count < ActiveConfig.NumPushingThreads * ActiveConfig.NumPushedPerThread)
+            {
+                int? x = _stack.Pop();
+                if (x.HasValue)
+                {
+                    _popped.Add(x.Value);
+                    _poppedInOrder.Add(x.Value);
+                }
+            }
         }
 
         private void PrepareForNewConfig()
         {
-            _stack = new LockFreeStack();
-            _popped = new List<int>();
+            _stack = new NaiveLockFreeStack();
+            _popped = new HashSet<int>();
+            _poppedInOrder = new List<int>();
+            _pushingThreadFinished = new MemoryOrdered<bool>();
+        }        
+        
+        private void SetupActiveConfig()
+        {
+            var threadEntries = new List<Action>();
+            for(int i = 0; i < ActiveConfig.NumPushingThreads; ++i)
+            {
+                threadEntries.Add(MakePushingThread(i, ActiveConfig.NumPushedPerThread));
+            }
+            for(int i = 0; i < ActiveConfig.NumPoppingThreads; ++i)
+            {
+                threadEntries.Add(MakePoppingThread());
+            }
+            ThreadEntries = threadEntries;
         }
 
         public bool SetNextConfiguration()
         {
             PrepareForNewConfig();
             bool moreConfigurations = _configs.MoveNext();
+            if(ActiveConfig != null)
+            {
+                SetupActiveConfig();
+            }
             return moreConfigurations;
         }
     }
