205a5bbdab327570dde7d2eb5f9ec4054a845509
Nick Nash <nicholas.nash@gmail.com>
2017-04-20 21:40:42 +0100

    Add longer wait cycle deadlocks

--- Examples/Deadlock.cs
+++ Examples/Deadlock.cs
@@ -13,16 +13,36 @@ namespace RelaSharp.Examples
         public bool ExpectedToFail => ActiveConfig.ExpectedToFail;
         private static TestEnvironment TE = TestEnvironment.TE;
 
-        private IEnumerator<SimpleConfig> _configs;
-        private SimpleConfig ActiveConfig => _configs.Current;
+        private IEnumerator<Config> _configs;
+        private Config ActiveConfig => _configs.Current;
 
-        private Object lockObjectA = new object();
-        private Object lockObjectB = new object();
+        private class Config
+        {
+            public readonly string Description;
+            public readonly int NumThreads;
+            public readonly bool ExpectedToFail;
+
+            public object[] LockObjects;
+            public Config(string description, int numThreads, bool expectedToFail)
+            {
+                Description = description;
+                NumThreads = numThreads;
+                ExpectedToFail = expectedToFail;
+                LockObjects = new Object[numThreads];
+                for(int i = 0; i < numThreads; ++i)
+                {
+                    LockObjects[i] = new Object();
+                }
+            }
+        }
 
         public Deadlock()
         {
-            ThreadEntries = new List<Action> {Thread1,Thread2};
-            var configList = new List<SimpleConfig>{new SimpleConfig("Simple deadlock", MemoryOrder.Relaxed, true)};
+            var configList = new List<Config>{new Config("1 thread: deadlock impossible", 1, false),
+                                              new Config("2 threads, with possible cyclic wait: deadlock expected", 2, true),
+                                              new Config("4 threads, with possible cyclic wait: deadlock expected", 4, true),
+                                              new Config("8 threads, with possible cyclic wait: deadlock expected", 8, true)
+                                              };
             _configs = configList.GetEnumerator();
         }
 
@@ -31,21 +51,6 @@ namespace RelaSharp.Examples
             PrepareForNewConfig();
         }
 
-        public void Thread1()
-        {
-            RMonitor.Enter(lockObjectA);
-            RMonitor.Enter(lockObjectB);
-            RMonitor.Exit(lockObjectB);
-            RMonitor.Exit(lockObjectA);
-        }
-
-        public void Thread2()
-        {
-            RMonitor.Enter(lockObjectB);
-            RMonitor.Enter(lockObjectA);
-            RMonitor.Exit(lockObjectA);
-            RMonitor.Exit(lockObjectB);
-        }
         public void OnBegin()
         {
         }
@@ -57,10 +62,39 @@ namespace RelaSharp.Examples
         {
         }
 
+        private Action MakeThread(int idx)
+        {
+            return () => LockingThread(idx);
+        }
+
+        private void LockingThread(int idx)
+        {
+            var myLock = ActiveConfig.LockObjects[idx];
+            var nextLock = ActiveConfig.LockObjects[(idx + 1) % ActiveConfig.NumThreads];
+            RMonitor.Enter(myLock);
+            RMonitor.Enter(nextLock);
+            RMonitor.Exit(nextLock);
+            RMonitor.Exit(myLock);
+        }
+
+        private void SetupActiveConfig()
+        {
+            var threadEntries = new List<Action>();
+            for(int i = 0; i < ActiveConfig.NumThreads; ++i)
+            {
+                threadEntries.Add(MakeThread(i));
+            }
+            ThreadEntries = threadEntries;                
+        }
+        
         public bool SetNextConfiguration()
         {
             PrepareForNewConfig();
             bool moreConfigurations = _configs.MoveNext();
+            if(ActiveConfig != null)
+            {
+                SetupActiveConfig();
+            }
             return moreConfigurations;
         }
     }
