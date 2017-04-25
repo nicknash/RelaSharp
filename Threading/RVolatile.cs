f8474e88a34c09406d664e289133cb595895868a
Nick Nash <nicholas.nash@gmail.com>
2017-04-25 07:16:35 +0100

    Add spike of ref-style RVolatile interface

--- Threading/RVolatile.cs
+++ Threading/RVolatile.cs
@@ -2,16 +2,34 @@ using System.Runtime.CompilerServices;
 
 namespace RelaSharp
 {
-    static class RVolatile<T>
+    static class RVolatile
     {
-        public static T Read(Atomic<T> data, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+        public static T Read<T>(ref Atomic<T> data, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
         {
+            if(data == null)
+            {
+                data = new Atomic<T>();
+            }
             return data.Load(MemoryOrder.Acquire, memberName, sourceFilePath, sourceLineNumber);
         }
 
-        public static void Write(Atomic<T> data, T newValue, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+        public static void Write<T>(ref Atomic<T> data, T newValue, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
         {
+            if(data == null)
+            {
+                data = new Atomic<T>();
+            }
             data.Store(newValue, MemoryOrder.Release, memberName, sourceFilePath, sourceLineNumber);
         }
+
+        public static long Read(ref Atomic64 data, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            if(data == null)
+            {
+                data = new Atomic64();
+            }
+            return data.Load(MemoryOrder.Acquire, memberName, sourceFilePath, sourceLineNumber);
+        }
+
     }
 }
