4f838eda2e88608ab55fb12069372802931555f3
Nick Nash <nicholas.nash@gmail.com>
2017-04-28 22:52:19 +0100

    Move CLRAtomicFactory out of RVolatile into CLRAtomic

--- CLR/CLRAtomic.cs
+++ CLR/CLRAtomic.cs
@@ -27,5 +27,34 @@ namespace RelaSharp.CLR
             __internal = new Atomic64();
         }
     }
+    static class CLRAtomicFactory
+    {
+        public static Atomic<T> GetAtomic<T>(ref CLRAtomic<T> data)
+        {
+            if(data == null)
+            {
+                data = new CLRAtomic<T>();
+            }
+            return data.__internal;
+        }
+
+        public static Atomic32 GetAtomic32(ref CLRAtomic32 data)
+        {
+            if(data == null)
+            {
+                data = new CLRAtomic32();
+            }
+            return data.__internal;
+        }
+  
+        public static Atomic64 GetAtomic64(ref CLRAtomic64 data)
+        {
+            if(data == null)
+            {
+                data = new CLRAtomic64();
+            }
+            return data.__internal;
+        }
+    }
 
 }
