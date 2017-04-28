e352bef696003b342024c8e384cc1a4503792f7c
Nick Nash <nicholas.nash@gmail.com>
2017-04-28 07:15:17 +0100

    Add 32/64-bit Read/Write functions

--- Threading/RVolatile.cs
+++ Threading/RVolatile.cs
@@ -45,5 +45,28 @@ namespace RelaSharp
             var atomic = CLRAtomicFactory.GetAtomic(ref data);
             atomic.Store(newValue, MemoryOrder.Release, memberName, sourceFilePath, sourceLineNumber);
         }
+       public static int Read(ref CLRAtomic32 data, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            var atomic = CLRAtomicFactory.GetAtomic32(ref data);
+            return atomic.Load(MemoryOrder.Acquire, memberName, sourceFilePath, sourceLineNumber);
+        }
+
+        public static void Write(ref CLRAtomic32 data, int newValue, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            var atomic = CLRAtomicFactory.GetAtomic32(ref data);
+            atomic.Store(newValue, MemoryOrder.Release, memberName, sourceFilePath, sourceLineNumber);
+        }       
+
+        public static long Read(ref CLRAtomic64 data, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            var atomic = CLRAtomicFactory.GetAtomic64(ref data);
+            return atomic.Load(MemoryOrder.Acquire, memberName, sourceFilePath, sourceLineNumber);
+        }
+
+        public static void Write(ref CLRAtomic64 data, long newValue, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            var atomic = CLRAtomicFactory.GetAtomic64(ref data);
+            atomic.Store(newValue, MemoryOrder.Release, memberName, sourceFilePath, sourceLineNumber);
+        }
     }
 }
