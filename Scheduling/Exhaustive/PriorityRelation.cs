573af112f65c16aacc568afb67826bf1c63d478b
Nick Nash <nicholas.nash@gmail.com>
2017-06-04 11:50:20 +0100

    Exhaustive scheduler: Use a simple quadratic time algorithm for computing schedulable threads. The intention is always that the number of threads is small (at very most 10) in any case.

--- Scheduling/Exhaustive/PriorityRelation.cs
+++ Scheduling/Exhaustive/PriorityRelation.cs
@@ -5,28 +5,22 @@ namespace RelaSharp.Scheduling.Exhaustive
         private readonly int _numThreads;
         private readonly bool[,] _hasPriority; // _hasPriority[x, y] = true <=> x will not be scheduled when y is enabled.
         private readonly ThreadSet _ready;
-        private int[] _priorityOver;
 
         public PriorityRelation(int numThreads)
         {
             _numThreads = numThreads;
             _hasPriority = new bool[numThreads, numThreads];
-            _ready = new ThreadSet(numThreads);
-            _priorityOver = new int[numThreads];
-            ComputeReady();
         }
 
         public void GivePriorityOver(int x, ThreadSet threads)
         {
             for (int i = 0; i < _numThreads; ++i)
             {
-                if(threads.Contains(i))
+                if(threads[i] && i != x)
                 {
                     _hasPriority[x, i] = true;
                 }
             }
-            _priorityOver[x] += threads.NumElems;
-            ComputeReady();
         }
 
         public void RemovePriorityOf(int x)
@@ -34,32 +28,31 @@ namespace RelaSharp.Scheduling.Exhaustive
             for (int i = 0; i < _numThreads; ++i)
             {
                 _hasPriority[i, x] = false;
-                if(_priorityOver[i] > 0)
-                {
-                    _priorityOver[i]--;
-                }
-            }
-            ComputeReady();
-        }
-
-        private void ComputeReady()
-        {
-            _ready.Clear();
-            for (int i = 0; i < _numThreads; ++i)
-            {
-                if (_priorityOver[i] == 0)
-                {
-                    _ready.Add(i);
-                }
             }
         }
 
         public ThreadSet GetSchedulableThreads(ThreadSet enabled)
         {
-            var result = new ThreadSet(_numThreads);
-            result.ReplaceWith(enabled);
-            result.IntersectWith(_ready);
-            return result;
+            var admissable = new ThreadSet(_numThreads);
+            admissable.ReplaceWith(enabled);
+            for(int i = 0; i < _numThreads; ++i)
+            {
+                if(!admissable[i])
+                {
+                    continue;
+                }
+                for(int j = 0; j < _numThreads; ++j)
+                {
+                    // Thread i cannot be scheduled if an enabled thread
+                    // has priority over it.
+                    if(_hasPriority[i, j] && enabled[j])
+                    {
+                        admissable.Remove(i);
+                        break;
+                    }
+                }
+            }
+            return admissable;
         }
     }
 }
